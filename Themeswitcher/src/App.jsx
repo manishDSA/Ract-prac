cb73fcddde89822d12624ac64a978802a2216f5e
manishDSA <manishsolanki7879@gmail.com>
2024-10-12 17:42:01 +0530

    UpdateIn Code

--- Themeswitcher/src/App.jsx
+++ Themeswitcher/src/App.jsx
@@ -29,12 +29,12 @@ useEffect(()=>{
     <div className="flex flex-wrap min-h-screen items-center">
       <div className="w-full">
         <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-          {/* {theme button} */}
+          
           <ThemeBtn/>
         </div>
 
         <div className="w-full max-w-sm mx-auto">
-       {/* {Card} */}
+       
        <Card/>
         </div>
       </div>
